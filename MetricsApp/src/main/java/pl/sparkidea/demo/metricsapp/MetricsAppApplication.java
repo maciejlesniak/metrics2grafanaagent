fbd90e636772640d3219599a04d8015f033e3be7
Maciek Leśniak <maciek@sparkmedia.pl>
2024-09-11 23:08:28 +0200

    init

---
+++ MetricsApp/src/main/java/pl/sparkidea/demo/metricsapp/MetricsAppApplication.java
@@ -0,0 +1,13 @@
+package pl.sparkidea.demo.metricsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetricsAppApplication.class, args);
+    }
+
+}
---
+++ MetricsApp/src/test/java/pl/sparkidea/demo/metricsapp/MetricsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sparkidea.demo.metricsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
